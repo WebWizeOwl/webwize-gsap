1d752aae10ec58ca4479fc87e67e3ea667b211a0
WebWizeOwl <contact@webwize.fr>
2024-08-01 09:36:49 +0200

    Update script.js
    GSAP animation test - delayed-scale

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 console.log("hello world")
 
-gsap.from("#upslide-fadein", {opacity: 0, y: "6rem", duration: 1})
+// Anime l'affichage de l'élément en le faisant apparaitre en glissant vers le haut
+gsap.from(
+  "#upslide-fadein", {
+    opacity: 0,
+    y: "6rem",
+    duration: 1,
+    ease: "power4.out"}
+)
+
+gsap.from("#delayed-scale",
+  {
+    delay: 1,
+    scale: 0,
+    duration: 1,
+  }
+)
+
+
