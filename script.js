5119aed59f7ebd9e34861a050cdf273acf43f51b
WebWizeOwl <contact@webwize.fr>
2024-08-01 14:42:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,114 +1,86 @@
 window.addEventListener('load', function() {
-
   
   // Timelines de chargement de page
   let loadTimeline = gsap.timeline();
   let loopTimelineBreathing = gsap.timeline({repeat: -1, yoyo: true, paused: true});
-  $(".section-staggered_fade_in_animation").each(function(){
-    let staggeredFadeInTimeline = gsap.timeline(
-    {
-      scrollTrigger: 
-        {
-          trigger: $(this), 
-          start: "top bottom", 
-          end: "top 65%", 
-          markers: false,
-          // play pause resume reset restart complete reverse none
-          toggleActions: "none play none reset",
-        }
-    }
-  )
-     staggeredFadeInTimeline.from(
-    $(this).find(".staggered_fadein-wrapper"), {
+  
+  // Staggered fade-in animations for multiple sections
+  $(".section-staggered_fade_in_animation").each(function() {
+    let staggeredFadeInTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: $(this),
+        start: "top bottom",
+        end: "top 65%",
+        markers: false,
+        toggleActions: "none play none reset",
+      }
+    });
+    
+    staggeredFadeInTimeline.from($(this).find(".staggered_fadein-wrapper"), {
       opacity: 0,
       scale: 0.7,
       duration: 0.5,
-      stagger: 
-      {
+      stagger: {
         each: 0.25,
         from: "center",
       }
-    }
-  )
-  })
+    });
+  });
   
-  let floatingImageCoupleTimeline = gsap.timeline(
-    {
+  // Floating image couple animations for multiple sections
+  $(".section_floating_image_couple").each(function() {
+    let floatingImageCoupleTimeline = gsap.timeline({
       scrollTrigger: {
-        trigger: ".section_floating_image_couple",
+        trigger: $(this),
         start: "top 30%",
         end: "bottom",
         markers: false,
         scrub: 2,
-      }    
-    }
-  )
+      }
+    });
+    
+    floatingImageCoupleTimeline.to($(this).find(".floating-image_1"), {
+      y: "-6rem"
+    });
+    
+    floatingImageCoupleTimeline.to($(this).find(".floating-image_2"), {
+      y: "-3rem"
+    }, 0);
+  });
   
-  // Anime l'affichage de l'élément en le faisant apparaitre en glissant vers le haut
-  loadTimeline.from(
-    ".headline-component", 
-    {
-      opacity: 0,
-      y: "6rem",
-      duration: 1,
-      ease: "power4.out",
-    }
-  );
+  // Headline component animation
+  loadTimeline.from(".headline-component", {
+    opacity: 0,
+    y: "6rem",
+    duration: 1,
+    ease: "power4.out",
+  });
   
-  // Apparition d'image
-  loadTimeline.from(
-    ".cours_nos-cours-d-equitation_image",
-    {
-      opacity: 0,
-      borderRadius: "50%",
-      duration: 1,
-      ease: "power4.out",
-      onComplete: loopTimelineBreathing.play(),
-    },
-    "<0.5"
-  );
-
-
-  // Apparition scalin-fadein
-  gsap.from(
-    ".scaling-fadein",
-    {
+  // Image appearance animation with looping breathing effect
+  loadTimeline.from(".big_image_appearance-wrapper", {
+    opacity: 0,
+    borderRadius: "50%",
+    duration: 1,
+    ease: "power4.out",
+    onComplete: loopTimelineBreathing.play(),
+  }, "<0.5");
+  
+  // Scaling fade-in animations for multiple elements
+  $(".scaling-fadein").each(function() {
+    gsap.from($(this), {
       scale: 0,
       opacity: 0,
-    },
-    1.5
-  );
+      duration: 1.5,
+      ease: "power4.out",
+    });
+  });
   
-  loopTimelineBreathing.to(
-    ".cours_nos-cours-d-equitation_image",
-    {
-      y: "-1rem",
-      duration: 2,
-      ease: "sine.inOut",
-    }
-  )
+  // Looping breathing effect for multiple images
+  loopTimelineBreathing.to(".big_image_appearance-wrapper", {
+    y: "-1rem",
+    duration: 2,
+    ease: "sine.inOut",
+  });
 
- 
-
-  floatingImageCoupleTimeline.to(
-    ".floating-image_1",
-    {
-      y: "-6rem"
-    }
-  )
-
-  floatingImageCoupleTimeline.to(
-    ".floating-image_2",
-    {
-      y: "-3rem"
-    },
-    0
-  )
-
-  
 });
-
-
-
-
 
