35081b14b198564050578c135545731a6d326966
WebWizeOwl <contact@webwize.fr>
2024-08-01 10:15:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 window.addEventListener('load', function() {
   // Anime l'affichage de l'élément en le faisant apparaitre en glissant vers le haut
-  gsap.from("#upslide-fadein", {
-    opacity: 0,
-    y: "6rem",
-    duration: 1,
-    ease: "power4.out"
-  });
+  gsap.from(
+    ".headline-component", 
+    {
+      opacity: 0,
+      y: "6rem",
+      duration: 1,
+      ease: "power4.out"
+    }
+  );
 
-  gsap.from("#delayed-scale", {
-    scale: 0,
-    duration: 1,
-    ease: "power4.out"
-  });
-
-  gsap.from(".cours_types-de-cours-proposes_item", {
-    scale: 0,
-    opacity: 0,
-    stagger: {
-      amount: 1,
-      from: "random"
+  gsap.fromTo(
+    ".scaling-button", 
+    {
+      scale: 0,
     },
-    duration: 0.5,
-    ease: "power4.out"
-  });
+    {
+      scale: 1,
+      duration: 1,
+      ease: "power4.out"
+    }
+  );
+
+  gsap.from(
+    ".cours_types-de-cours-proposes_item", 
+    {
+      scale: 0,
+      opacity: 0,
+      stagger: 
+      {
+        amount: 1,
+        from: "random"
+      },
+      duration: 0.5,
+      ease: "power4.out"
+    }
+  );
 });
