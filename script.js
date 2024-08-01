09a5a5602a9542c67331215ac455615946a1100f
WebWizeOwl <contact@webwize.fr>
2024-08-01 09:54:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ console.log("hello world")
 
 // Anime l'affichage de l'élément en le faisant apparaitre en glissant vers le haut
 gsap.from(
-  "#upslide-fadein", {
+  "#upslide-fadein", 
+  {
     opacity: 0,
     y: "6rem",
     duration: 1,
-    ease: "power4.out"}
+    ease: "power4.out"
+  }
 )
 
 gsap.from("#delayed-scale",
@@ -22,7 +24,11 @@ gsap.from(
   {
     scale: 0,
     opacity: 0,
-    stagger: {amount: 5, from: "random"},
+    stagger: 
+    {
+      amount: 5,
+      from: "random"
+    },
     duration: 1,
     ease: "power4.out"
   }
