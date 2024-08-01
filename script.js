a651fa4968190014483b070fb7c158c58286b68d
WebWizeOwl <contact@webwize.fr>
2024-08-01 15:46:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ window.addEventListener('load', function() {
     ".symetric-shrink",
     {
       scale: 0.9,
-      rotateY: 20,
+      rotationY: 20,
     }, 0
   )
 
@@ -108,7 +108,7 @@ window.addEventListener('load', function() {
     ".symetric-grow",
     {
       scale: 1.1,
-      rotateY: -20,
+      rotationY: -20,
     }, 0
   )
   
