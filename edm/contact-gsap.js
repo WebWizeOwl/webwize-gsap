29c9dbb86d8eef38fd61ae04b1ecec8fa31f4f58
WebWizeOwl <contact@webwize.fr>
2024-08-02 19:04:18 +0200

    Update contact-gsap.js

--- edm/contact-gsap.js
+++ edm/contact-gsap.js
@@ -1,1 +1,24 @@
+window.addEventListener('load', function() {
 
+  // ANIMATION NAVBAR BATTANT DE PORTE
+
+  let doorWingTimeline = gsap.timeline()
+  
+  doorWingTimeline.set(
+    ".navbar",
+    {
+      rotateY: -90,
+      backgroundColor: #482e33,
+    }
+  )
+
+  doorWingTimeline.to(
+    ".navbar",
+    {
+      rotateY: 0,
+      duration:2.5,
+      ease: "elastic.out(1,0.3)",
+    }
+  )
+  
+})
